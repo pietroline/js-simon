8c547e6285c2b1c98ba452af661f7986ca126a74
pietroline <pietrobarberi@hotmail.it>
2022-02-07 15:52:02 +0100

    aggiornamento

--- js/main.js
+++ js/main.js
@@ -18,6 +18,7 @@ for(let i=0; i<5; i++){
     numeri[i] = random(0,99);
 }
 
+console.log("numeri casuali: "+numeri)
 
 
 //append dell'arrray numeri sul DOM
@@ -34,9 +35,17 @@ setTimeout(function(){
 
     //prelevo i numeri ricordati dall'utente
     let numeriRicordati = [];
+    let numeriCorretti =[];
     for(let i=0; i<5; i++){
+
         numeriRicordati[i] = prompt(`Inserisci il ${i+1}° numero che ricordi`);
+        if(numeri.includes(numeriRicordati[i])){
+            numeriCorretti.push(numeriRicordati[i]);
+        }
     }
+
+    console.log("Numeri ricordati: " + numeriRicordati);
+    console.log("Numeri corretti: " + numeriCorretti);
 
 
 }, 2000);
